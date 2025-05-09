0bd03d704f4c767dc570a4145ef0e0112e955b62
spandana-225 <spandana21221@gmail.com>
2025-05-09 20:28:31 +0530

    Add files via upload

---
+++ foundations/projects/calculator-cli/calc/core/__init__.py
@@ -0,0 +1,6 @@
+def __init__(self,a,b):
+    self.a=a
+    self.b=b
+def __init__(self,mom,dad):
+    self.mom=mom
+    self.dad=dad
---
+++ foundations/projects/calculator-cli/calc/core/calculator.py
@@ -0,0 +1,5 @@
+from abc import abstractmethod,ABC
+
+class BaseCalculator(ABC):
+    def calculator(self):
+        pass
